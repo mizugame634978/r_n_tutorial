203a9ed8438f87138b4cf49209141249f76f018f
mizugame <ryouta1853@gmail.com>
2023-07-23 20:57:15 +0900

    useCallback,useEffectの第２引数について勉強

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,22 +3,19 @@ import { Footer } from "@/app/components/Footer";
 import { Main } from "@/app/components/Main";
 import { Header } from "@/app/components/Header";
 import styles from "./page.module.css";
-import {  useEffect, useState } from "react";
+import {  useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count,setCount]=useState(1);//useStateの返り値の配列の[0]にfoo,[1]にsetFooが入っている（分割代入）
-  //const array=useState(1);
-  //const count = array[0];
-  //const setCount = array[1];
 
-  const handleClick =(e)=>{//useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
-    setCount(count => count+1);//アロー関数(１行)
-    // setCount(function(count){
-    //   return count + 1;
-    // });
-    setCount(count => count+1);
+  const handleClick =useCallback(()=>{//useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
 
-  };
+    if(count < 10){
+      setCount(hiki => hiki+1);//アロー関数(１行),前回のhiki+1(返り値)の値をみて処理をするので、countがずっと１でもここは増え続ける
+      //引数countを受け取り、count+1を返す
+      //またsetCountはuseStateで生み出された関数なので自動的に引数には勝手にcountが入る
+    }
+  },[count]);
 
   useEffect(()=>{
     document.body.style.backgroundColor="lightblue";
