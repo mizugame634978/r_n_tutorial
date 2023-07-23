3e95456df230d5846dbd42156edc1f4638b2c337
mizugame <ryouta1853@gmail.com>
2023-07-23 18:40:49 +0900

    カウンターを設置してｂuseStateでカウントアップする

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,20 +3,26 @@ import { Footer } from "@/app/components/Footer";
 import { Main } from "@/app/components/Main";
 import { Header } from "@/app/components/Header";
 import styles from "./page.module.css";
-import { useCallback, useEffect } from "react";
+import {  useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick =useCallback((e)=>{//useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  const [count,setCount]=useState(1);//useStateの返り値の配列の[0]にfoo,[1]にsetFooが入っている（分割代入）
+  //const array=useState(1);
+  //const count = array[0];
+  //const setCount = array[1];
+
+  const handleClick =(e)=>{//useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
+    setCount(count => count+1);//アロー関数(１行)
+    // setCount(function(count){
+    //   return count + 1;
+    // });
+    setCount(count => count+1);
+
+  };
+
   useEffect(()=>{
-    console.log("マウント時");
     document.body.style.backgroundColor="lightblue";
     return()=>{
-      console.log("アンマウント時");
       document.body.style.backgroundColor="";
     }
   },[]);
@@ -28,12 +34,10 @@ export default function Home() {
         <title>this is title</title>
         <link rel="icon" href="./favicon.ico"/>
         <Header />
-        <a
-          href="/hoge"
-          onClick={handleClick}
-        >
+        <h1>{count}</h1>
+        <button onClick={handleClick}>
           ボタン
-        </a>
+        </button>
       <Main page="index" />
       <Footer />
     </div>
