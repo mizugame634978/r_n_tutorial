68800990490d99953c3318c547ba1112628f5ee7
mizugame <ryouta1853@gmail.com>
2023-07-23 23:27:43 +0900

    useStateで文字列と真偽値について解説

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,38 +3,54 @@ import { Footer } from "@/app/components/Footer";
 import { Main } from "@/app/components/Main";
 import { Header } from "@/app/components/Header";
 import styles from "./page.module.css";
-import {  useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-  const [count,setCount]=useState(1);//useStateの返り値の配列の[0]にfoo,[1]にsetFooが入っている（分割代入）
+  const [count, setCount] = useState(1); //useStateの返り値の配列の[0]にfoo,[1]にsetFooが入っている（分割代入）
 
-  const handleClick =useCallback(()=>{//useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
+  const [text, setText] = useState("");
+  const [isShow, setisShow] = useState(true);
 
-    if(count < 10){
-      setCount(hiki => hiki+1);//アロー関数(１行),前回のhiki+1(返り値)の値をみて処理をするので、countがずっと１でもここは増え続ける
+  const handleClick = useCallback(() => {
+    //useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
+
+    if (count < 10) {
+      setCount((hiki) => hiki + 1); //アロー関数(１行),前回のhiki+1(返り値)の値をみて処理をするので、countがずっと１でもここは増え続ける
       //引数countを受け取り、count+1を返す
       //またsetCountはuseStateで生み出された関数なので自動的に引数には勝手にcountが入る
     }
-  },[count]);
+  }, [count]);
 
-  useEffect(()=>{
-    document.body.style.backgroundColor="lightblue";
-    return()=>{
-      document.body.style.backgroundColor="";
+  const handleDisplay = useCallback(() => {
+    setisShow((hiki) => !hiki); //not(!)だからtrueだとfalseになる？
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("５文字以内にしてください");
+      return;
     }
-  },[]);
+    setText(e.target.value.trim());
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
-
-
     <div className={styles.container}>
-        <title>this is title</title>
-        <link rel="icon" href="./favicon.ico"/>
-        <Header />
-        <h1>{count}</h1>
-        <button onClick={handleClick}>
-          ボタン
-        </button>
+      <title>this is title</title>
+      <link rel="icon" href="./favicon.ico" />
+      <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示させる"}
+      </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
