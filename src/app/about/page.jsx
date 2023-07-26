7b2f64a2e34f8eadad990e5f453f620d1d01418c
mizugame <ryouta1853@gmail.com>
2023-07-26 20:08:32 +0900

    :sparkles: useMemoについて学びました

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,7 +8,7 @@ import { useInputArray } from "@/app/hooks/useInputArray";
 import { useBgLightBlue } from "@/app/hooks/useBgLightBlue";
 
 export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { doubleCounter, isShow, handleClick, handleDisplay } = useCounter();
   const { text, array, handleChange, handleAdd } = useInputArray();
   useBgLightBlue();
 
@@ -20,7 +20,7 @@ export default function About() {
       <button onClick={handleDisplay}>
         {isShow ? "非表示" : "表示させる"}
       </button>
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCounter}</h1> : null}
 
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>ついか</button>
--- src/app/hooks/useCounter.jsx
+++ src/app/hooks/useCounter.jsx
@@ -1,5 +1,5 @@
 
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 export const useCounter = () => {
   //自作のカスタムhooksではuseから関数名が始まる
@@ -7,7 +7,6 @@ export const useCounter = () => {
   const [isShow, setisShow] = useState(true);
   const handleClick = useCallback(() => {
     //useCallbackをつかうことで再読み込みの際に関数が再レンダリングされなくなる
-
     if (count < 10) {
       setCount((prevCount) => prevCount + 1); //アロー関数(１行),前回のprevCount+1(返り値)の値をみて処理をするので、countがずっと１でもここは増え続ける
       //引数countを受け取り、count+1を返す
@@ -15,8 +14,13 @@ export const useCounter = () => {
     }
   }, [count]);
 
+  const doubleCounter = useMemo(()=>{//reactのコンポーネント内で何かを定義するときは全てに対してuseMemoを使用
+    //関数ならuseCallback,ただしたまに使うとパフォーマンスが悪くなるが微々たるものなので気にしない
+    return count*2;
+  },[count]);//第２引数に入れた値が変化すると新しく作り直される
+
   const handleDisplay = useCallback(() => {
     setisShow((prevIsShow) => !prevIsShow); //not(!)だからtrueだとfalseになる？
   }, []);
-  return { count, isShow, handleClick, handleDisplay };
+  return { count,doubleCounter, isShow, handleClick, handleDisplay };
 };
